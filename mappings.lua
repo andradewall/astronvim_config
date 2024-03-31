edb7292dbe585248b1c93c494c714a4b16ed2fa6
Wallace Andrade <andradewall0@gmail.com>
2024-03-31 12:26:41 -0300

    feat: add neotest and neotest-pest

--- mappings.lua
+++ mappings.lua
@@ -32,6 +32,14 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>rn"] = {
+      function() require('neotest').run.run() end,
+      desc = "Run test for nearest function",
+    },
+    ["<leader>rf"] = {
+      function() require("neotest").run.run(vim.fn.expand("%")) end,
+      desc = "Run test for current file",
+    }
   },
   t = {
     -- ToggleTerm

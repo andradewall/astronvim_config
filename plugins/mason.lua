c9141521a0832fa5fb798b81ea2c3f817257fc19
Wallace Andrade <andradewall0@gmail.com>
2024-03-31 12:26:50 -0300

    feat: remove shellharden and luacheck

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -32,13 +32,11 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "shellcheck",
-        "shellharden",
         "tlint",
         "stylelint",
         "hadolint",
         "htmlhint",
         "biome",
-        "luacheck",
         "markdownlint",
         "pint"
       })
